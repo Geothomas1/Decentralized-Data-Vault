05afcac7c73b8239d953d1526a522e0c118c66d2
geothomas <geothomas@cet.ac.in>
2021-01-14 09:18:49 +0530

    admin login refactor,header,controller, admin.js

---
+++ api/controller/admin-controller.js
@@ -0,0 +1,22 @@
+'use strict';
+const operator = require('../utils/operator');
+
+exports.login = async(req, res) => {
+    console.log('Admin login', req.body);
+    if (req.body.username && req.body.password) {
+        if (req.body.username == 'admin' && req.body.password == 'admin') {
+            req.session.username = 'admin';
+            req.session.save(err => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.redirect('/admin/home');
+                }
+            });
+        }
+    } else {
+        console.log('Invalid format');
+        req.session.loginErr = true
+        return res.render('admin/login', { status: 0, loginErr: req.session.loginErr })
+    }
+};
--- api/routes/admin.js
+++ api/routes/admin.js
@@ -3,10 +3,16 @@ var admin = require('../controller/admin-controller');
 var router = express.Router();
 
 router.get('/login', (req, res) => {
-        res.render('admin/login', { loginErr: req.session.loginErr = false })
-    })
-    //router.post('/login', admin.login);
-
+    res.render('admin/login', { loginErr: req.session.loginErr = false })
+})
+router.post('/login', admin.login);
+router.get('/home', (req, res) => {
+    res.render('admin/home', { username: req.session.username })
+})
+router.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/admin/login');
+});
 
 
 module.exports = router;
